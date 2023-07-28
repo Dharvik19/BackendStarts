752698c08eb434212fd372f3acc9ef22eda15dbf
Dharvik19 <dharvik.badga22@gmail.com>
2023-07-28 18:23:37 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
-
+const fs = require('fs');
+const path = require('path');
 const server = http.createServer((req, res)=>{
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
-    res.write('<html>');
-    res.write('<head><title>First Page</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
+        let mydata = "";
+        fs.readFile("message.txt", {encoding:"utf-8"}, (err, data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("data from file", data);
+            mydata = data;
+            res.write('<html>');
+            res.write('<head><title>First Page</title></head>');
+            res.write(`<body>${mydata}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><div id="messages"></div></body>');
+            res.write('</html>');
+            return res.end();
+        })
+    
+    
     }else if(url === '/home'){
         res.write('<html>');
         res.write('<head><title>Home Page</title></head>');
@@ -27,12 +40,32 @@ const server = http.createServer((req, res)=>{
         res.write('</html>');
         return res.end();
     }
-    console.log('dharvik');
+
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end',()=>{
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            console.log(parseBody);
+            fs.writeFile('message.txt', message, err=>{
+                res.statusCode= 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+           
+        })
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
-    res.write('<head><title>Firs</title></head>');
-    res.write('<body><h1>Hello hellooooo</h1></body>');
-    res.write('</html>');
+            res.write('<head><title>First Page</title></head>');
+            
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><div id="messages"></div></body>');
+            res.write('</html>');
+            
     res.end();
 })
 
