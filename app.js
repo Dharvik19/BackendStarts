fa58f5583d40e2da696c9a9eb115e0cc37f6fe76
Dharvik19 <dharvik.badga22@gmail.com>
2023-07-31 21:40:58 +0530

    installed express

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
+// const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next)=>{
+    console.log("in the middleware");
+    next();//allows the request to continue in second middleware
+})
+app.use((req, res, next)=>{
+    console.log("in the second middleware");
+    // res.send('<h1>Hello from express</h1>') header text/html
+    res.send({key1:"1"});//header application/json
+})
+
 // const server = http.createServer(routes.handler);
 
-server.listen(3000);
+app.listen(3000);
